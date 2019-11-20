0a4d19aaea5017d7c8d8a3409aac85334bae999a
AustinHadden <vitovalletori@gmail.com>
2019-11-20 15:08:22 -0600

    added reducer and actions

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const FETCH_DATA_LOADING = "FETCH_DATA_LOADING";
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
+export const FETCH_DATA_FAILED = "FETCH_DATA_FAILED";
+
+export const dataLoading = () => ({ type: FETCH_DATA_LOADING });
+export const dataLoadSuccess = data => ({
+  type: FETCH_DATA_SUCCESS,
+  payload: data
+});
+export const dataLoadFailure = error => ({
+  type: FETCH_DATA_FAILED,
+  payload: error
+});
+
+export function fetchdata() {
+  return function(dispatch) {
+    dispatch(dataLoading());
+
+    return axios
+      .get(`https://silent-auction-be.herokuapp.com/api/auctions`)
+      .then(function(response) {
+        console.log('Server response', response);
+        dispatch(dataLoadSuccess(response.data.auctions));
+      })
+      .catch(error => dispatch(dataLoadFailure(error)));
+  };
+}
